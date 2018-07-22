877919f5846c87d6ff2e7210c9a1800b68ffb8a6
Raja Selvaraj <rajaselvaraj@Rajas-MacBook-Pro.local>
2018-07-22 14:53:22 -0500

    initial push

---
+++ src/test/java/com/funimation/BDD/Sample.java
@@ -0,0 +1,11 @@
+package com.funimation.BDD;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void profile() {
+		System.out.println("Profile 2: Smoke");
+	}
+}
---
+++ src/test/java/com/funimation/BDD/TestNGSamples.java
@@ -0,0 +1,17 @@
+package com.funimation.BDD;
+
+import org.testng.annotations.Test;
+
+public class TestNGSamples {
+	
+	@Test
+	public void demo() {
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void login() {
+		System.out.println("Login");
+	}
+
+}
